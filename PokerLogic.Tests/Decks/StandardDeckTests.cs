8fef588628086fc91e6faeeee9bcf09510b87826
middas <justin.middas@gmail.com>
2025-10-29 13:13:04 -0600

    Added more unit tests

--- PokerLogic.Tests/Decks/StandardDeckTests.cs
+++ PokerLogic.Tests/Decks/StandardDeckTests.cs
@@ -6,6 +6,82 @@ namespace PokerLogic.Tests.Decks
     internal class StandardDeckTests
     {
         [Test]
+        public void StandardDeck_CardsShouldBeInExpectedOrder()
+        {
+            // Arrange
+            StandardDeck deck = new();
+            Stack<Card> expectedOrder = [];
+            foreach (Constants.Suit suit in Enum.GetValues<Constants.Suit>())
+            {
+                foreach (Constants.Rank rank in Enum.GetValues<Constants.Rank>())
+                {
+                    expectedOrder.Push(new Card(suit, rank));
+                }
+            }
+            // Act
+            IEnumerable<Card> actualCards = deck.GetCards();
+            // Assert
+            Assert.That(actualCards, Is.EqualTo(expectedOrder), "The cards should be in the expected order.");
+        }
+
+        [Test]
+        public void StandardDeck_Draw_MoreThanAvailableCards_ShouldThrowException()
+        {
+            // Arrange
+            StandardDeck deck = new();
+            int drawCount = 60;
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => deck.Draw(drawCount));
+            Assert.That(ex.Message, Does.Contain("Count must be non-negative and less than or equal to the number of cards in the deck."));
+        }
+
+        [Test]
+        public void StandardDeck_Draw_NegativeCount_ShouldThrowException()
+        {
+            // Arrange
+            StandardDeck deck = new();
+            int drawCount = -5;
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => deck.Draw(drawCount));
+            Assert.That(ex.Message, Does.Contain("Count must be non-negative and less than or equal to the number of cards in the deck."));
+        }
+
+        [Test]
+        public void StandardDeck_Draw_ShouldReturnCorrectNumberOfCards()
+        {
+            // Arrange
+            StandardDeck deck = new();
+            int drawCount = 5;
+            // Act
+            IEnumerable<Card> drawnCards = deck.Draw(drawCount);
+            // Assert
+            Assert.That(drawnCards.Count(), Is.EqualTo(drawCount), $"Drawing {drawCount} cards should return {drawCount} cards.");
+            Assert.That(deck.GetCards().Count(), Is.EqualTo(52 - drawCount), $"The deck should have {52 - drawCount} cards remaining after drawing {drawCount} cards.");
+        }
+
+        [Test]
+        public void StandardDeck_Reset_ShouldRestoreOriginalOrder()
+        {
+            // Arrange
+            StandardDeck deck = new();
+            deck.Shuffle();
+            _ = deck.Draw(5);
+            // Act
+            deck.Reset();
+            IEnumerable<Card> resetOrder = deck.GetCards();
+            Stack<Card> expectedOrder = [];
+            foreach (Constants.Suit suit in Enum.GetValues<Constants.Suit>())
+            {
+                foreach (Constants.Rank rank in Enum.GetValues<Constants.Rank>())
+                {
+                    expectedOrder.Push(new Card(suit, rank));
+                }
+            }
+            // Assert
+            Assert.That(resetOrder, Is.EqualTo(expectedOrder), "The order and number of cards should be restored to the original after resetting.");
+        }
+
+        [Test]
         public void StandardDeck_ShouldContain52UniqueCards()
         {
             // Arrange
@@ -34,25 +110,6 @@ namespace PokerLogic.Tests.Decks
         }
 
         [Test]
-        public void StandardDeck_CardsShouldBeInExpectedOrder()
-        {
-            // Arrange
-            StandardDeck deck = new();
-            Stack<Card> expectedOrder = [];
-            foreach (Constants.Suit suit in Enum.GetValues<Constants.Suit>())
-            {
-                foreach (Constants.Rank rank in Enum.GetValues<Constants.Rank>())
-                {
-                    expectedOrder.Push(new Card(suit, rank));
-                }
-            }
-            // Act
-            IEnumerable<Card> actualCards = deck.GetCards();
-            // Assert
-            Assert.That(actualCards, Is.EqualTo(expectedOrder), "The cards should be in the expected order.");
-        }
-
-        [Test]
         public void StandardDeck_Shuffle_ShouldRandomizeCardOrder()
         {
             // Arrange
@@ -66,41 +123,6 @@ namespace PokerLogic.Tests.Decks
             Assert.That(shuffledOrder, Is.Not.EqualTo(originalOrder), "The order of cards should be different after shuffling.");
             Assert.That(shuffledOrder.Count(), Is.EqualTo(52), "The deck should still contain 52 cards after shuffling.");
             Assert.That(shuffledOrder.Distinct().Count(), Is.EqualTo(52), "All cards in the deck should still be unique after shuffling.");
-        }
-
-        [Test]
-        public void StandardDeck_Reset_ShouldRestoreOriginalOrder()
-        {
-            // Arrange
-            StandardDeck deck = new();
-            deck.Shuffle();
-            _ = deck.Draw(5);
-            // Act
-            deck.Reset();
-            IEnumerable<Card> resetOrder = deck.GetCards();
-            Stack<Card> expectedOrder = [];
-            foreach (Constants.Suit suit in Enum.GetValues<Constants.Suit>())
-            {
-                foreach (Constants.Rank rank in Enum.GetValues<Constants.Rank>())
-                {
-                    expectedOrder.Push(new Card(suit, rank));
-                }
-            }
-            // Assert
-            Assert.That(resetOrder, Is.EqualTo(expectedOrder), "The order and number of cards should be restored to the original after resetting.");
-        }
-
-        [Test]
-        public void StandardDeck_Draw_ShouldReturnCorrectNumberOfCards()
-        {
-            // Arrange
-            StandardDeck deck = new();
-            int drawCount = 5;
-            // Act
-            IEnumerable<Card> drawnCards = deck.Draw(drawCount);
-            // Assert
-            Assert.That(drawnCards.Count(), Is.EqualTo(drawCount), $"Drawing {drawCount} cards should return {drawCount} cards.");
-            Assert.That(deck.GetCards().Count(), Is.EqualTo(52 - drawCount), $"The deck should have {52 - drawCount} cards remaining after drawing {drawCount} cards.");
         }
     }
 }
